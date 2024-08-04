23a17a67d3a187f633094dae64d3ea5aa04b2e9b
Giorgi Cxovrebashvili <giorgicxovrebashvili@Giorgis-MacBook-Air.local>
2024-08-05 00:56:21 +0400

    final corrections

--- src/common/assets/index.ts
+++ src/common/assets/index.ts
@@ -1,3 +1,3 @@
-export { default as filter } from './Image/filter.png';
-export { default as mode } from './Image/mode.png';
-export { default as search } from './Image/search.png';
+export { default as filter } from "./Image/filter.png";
+export { default as mode } from "./Image/mode.png";
+export { default as search } from "./Image/search.png";
